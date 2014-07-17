a2695d29ee398cc54108b15498ffec1fdfb97113
Kelton Person <kelton.person@gmail.com>
2014-07-17 17:17:35 -0500

    commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+gem 'sinatra'
+gem 'thin'
+gem "rest-client", "~> 1.6.7"
+gem 'rack-cors', :require => 'rack/cors'
---
+++ app.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+use Rack::MethodOverride
+
+set :protection, :origin_whitelist => ['*']
+set :public_folder, File.expand_path("..", __FILE__) + "/UI"
+
+get '/' do
+  "Hello World"
+end
+
+
+before do
+  headers 'Access-Control-Allow-Origin' => '*', 'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+end
