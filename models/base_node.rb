5c9a0e3301eac896ae55b0cb77bf03f08c786e4a
Kelton Person <kelton.person@gmail.com>
2014-07-17 20:06:44 -0500

    added dao methods

--- models/base_node.rb
+++ models/base_node.rb
@@ -1,13 +1,23 @@
 class BaseNode
 
-  attr_accessor :child_nodes, :id
+  attr_accessor :child_nodes, :id, :name
 
-  def initialize(name, child_nodes)
+  def initialize(id, name, child_nodes)
     @name = name
     @child_nodes = child_nodes
+    @id = id
   end
 
   def run
+  end
+
+  def to_hash
+    child_hash = @child_nodes.collect{|x| x.to_hash }
+    {
+      'name' => @name,
+      'child_nodes' => child_hash,
+      'id' => id
+    }
   end
 
 end
--- models/flow_model.rb
+++ models/flow_model.rb
@@ -1,14 +1,27 @@
+require_relative 'action_node'
+require_relative 'endpoint_node'
+
 class FlowModel
 
   attr_accessor :name, :id, :initial_node_id
 
-  def initialize(name, id, initial_node_id)
+  def initialize(id, name, initial_node)
     @name = name
     @id = id
-    @initial_node_id = initial_node_id
+    @initial_node = initial_node
   end
 
-  def run
+  def to_hash
+    {
+      'id' => @id,
+      'name' => @name,
+      'initial_node' => @initial_node.to_hash
+    }
   end
+
+  def self.from_hash(hash)
+    FlowModel.new(hash['id'], hash['name'], hash['initial_node']['node_type'] == 'endpoint' ? EndpointNode.from_hash(hash['initial_node']) : ActionNode.from_hash(hash['initial_node']) )
+  end
+
 
 end
