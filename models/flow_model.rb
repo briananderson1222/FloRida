173a5ed8cd58d2c01060274a1613475e53bc9a57
Kelton Person <kelton.person@gmail.com>
2014-07-18 08:53:44 -0500

    whatever

--- models/flow_model.rb
+++ models/flow_model.rb
@@ -1,5 +1,6 @@
 require_relative 'action_node'
 require_relative 'endpoint_node'
+require 'json'
 
 class FlowModel
 
@@ -18,6 +19,40 @@ class FlowModel
       'initial_node' => @initial_node.to_hash
     }
   end
+
+  def print_nodes
+    FlowModel.print_helper(@initial_node)
+  end
+
+  def self.print_helper(node)
+    hash = node.to_hash
+    hash_child_nodes = hash.delete('child_nodes')
+    puts hash
+    node.child_nodes.each do |x|
+      FlowModel.print_helper(x)
+    end
+  end
+
+  def flat
+    big_arr = []
+    addition =  []
+    big_arr << addition
+    FlowModel.flat_helper(@initial_node, big_arr, addition, nil)
+    big_arr
+  end
+
+  def self.flat_helper(node, big_arr, addition, parent_id)
+    hash = node.to_hash
+    hash['parent_id'] = parent_id
+    hash_child_nodes = hash.delete('child_nodes')
+    addition << hash
+    new_addition = []
+    big_arr << new_addition
+    node.child_nodes.each do |x|
+      FlowModel.flat_helper(x, big_arr, new_addition, x.id)
+    end
+  end
+
 
   def self.from_hash(hash)
     FlowModel.new(hash['id'], hash['name'], hash['initial_node']['node_type'] == 'endpoint' ? EndpointNode.from_hash(hash['initial_node']) : ActionNode.from_hash(hash['initial_node']) )
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require_relative '../models/flow_model'
+require_relative '../models/endpoint_node'
+require_relative '../models/action_node'
